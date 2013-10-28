fc93d44ce5f0239d89cc41c04db031e322776b6a
Bruno Cauet <brunocauet@gmail.com>
2013-10-29 00:52:10 +0100

    More configuration, only example file given

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ def create_app():
         config = yaml.load(f)
 
     app = Flask(__name__)
+    app.debug = app.config['DEBUG']
+    app.secret_key = app.config['SECRET_KEY']
     app.config.update(config)
+
     db.init_app(app)
     return app
 
