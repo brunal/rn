4a2281c764d5c22036eeb5e725b90dcda2aece1b
Bruno Cauet <bruno.cauet@dolead.com>
2014-01-24 10:58:57 +0100

    tools: fix users name

--- tools.py
+++ tools.py
@@ -3,10 +3,11 @@
 """
 Small functions for convenience
 """
+import csv
+
 from main import *
 from models import *
 import login
-
 
 app = create_app()
 app.test_request_context().push()
@@ -28,3 +29,31 @@ def add_users():
     map(db.session.add, [v, r, b])
     db.session.commit()
     print u"ajouté :", v, r, b
+
+def fix_name():
+    fname = 'volontaires.csv'
+    with open(fname, 'rb') as f:
+        content = csv.reader(f, delimiter='\t')
+        for nom, prenom, _, email in content:
+            u = User.query.filter_by(email=email).first()
+            if u is None:
+                print "no account"
+                continue
+
+            new_name = fix_name_format(prenom, nom)
+            print u"%s -> %s" % (u.name, new_name)
+            u.name = new_name
+        db.session.commit()
+
+def fix_name_format(prenom, nom):
+    name_parts = nom.split()
+    new_name = [prenom]
+    for np in name_parts:
+        np = np[0].upper() + np[1:].lower()
+        if np == u'De':
+            np = u'de'
+        elif np == u'Du':
+            np = u'du'
+        new_name.append(np)
+    new_name = [n.decode('utf8') for n in new_name]
+    return ' '.join(new_name)
