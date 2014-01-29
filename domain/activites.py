a457d9c9888fa02ed181fa82bd0368a376d2a4d4
Bruno Cauet <brunocauet@gmail.com>
2014-01-29 21:31:39 +0100

    Export description in csv

--- domain/activites.py
+++ domain/activites.py
@@ -9,10 +9,11 @@ def to_csv(activites):
     csv_activites = StringIO()
     w = ucsv.writer(csv_activites)
     # headers
-    w.writerow(['Jour', u'Début', 'Fin', 'Nom', 'Lieu', 'Responsable', 'Email'])
+    w.writerow(['Jour', u'Début', 'Fin', 'Nom', 'Lieu', 'Responsable', 'Email', 'Description'])
     for a in activites:
         w.writerow([to_date(a.debut), to_time(a.debut), to_time(a.fin),
                     a.nom, a.lieu,
-                    a.responsable.user.name, a.responsable.user.email])
+                    a.responsable.user.name, a.responsable.user.email,
+                    a.description])
 
     return csv_activites.getvalue()
