0efe58ea038da35bdcdfbe94789b494691540df5
Bruno Cauet <bruno.cauet@dolead.com>
2014-02-05 11:03:17 +0100

    min/max/mean stats on assignement status page

--- domain/assignement.py
+++ domain/assignement.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
-import models
 from datetime import timedelta
+from collections import defaultdict
+
+import models
 
 
 class Stats(object):
@@ -17,3 +19,11 @@ class Stats(object):
     @property
     def avg_help_time(self):
         return self.tot_help_time / self.vols
+
+    def assign_values(self):
+        # this could be done in 3 queries... I suck at SQLalchemy
+        ass_by_person = defaultdict(timedelta)
+        for ass in models.Assignement.query.all():
+            ass_by_person[ass.volontaire] += ass.activite.fin - ass.activite.debut
+        times = ass_by_person.values()
+        return min(times), max(times), sum(times, timedelta()) / len(times)
